d39eafca5fa03cb81f7f1ee04c5c425410522336
Ryan Sullenberger <soolr@Ryans-MacBook-Pro.local>
2017-04-24 21:33:25 -0400

    initial commit

---
+++ cvxpy_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 20:28:04 2017
+
+@author: soolr
+"""
+
+import cvxpy as cvx
+import numpy as np
+
+# Problem data.
+m = 50
+n = 10
+np.random.seed(1)
+A = np.random.randn(m, n)
+b = np.random.randn(m)
+
+def cvxit(A,b):
+    # Construct the problem.
+    x = cvx.Variable(A.shape[1])
+    objective = cvx.Minimize(cvx.sum_squares(A*x - b))
+    constraints = [5 <= x, x <= 10]
+    prob = cvx.Problem(objective, constraints)
+    
+    # The optimal objective is returned by prob.solve().
+    result = prob.solve()
+    # The optimal value for x is stored in x.value.
+    #print(x.value)
+    # The optimal Lagrange multiplier for a constraint
+    # is stored in constraint.dual_value.
+    #print(constraints[0].dual_value)
+    return np.array(x.value)
+
+X = cvxit(A,b)
